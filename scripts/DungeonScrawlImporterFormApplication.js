84a59c523616676e222dec05b5854825c2a58eea
Pepijn <pepijnsietsema@gmail.com>
2023-01-08 10:32:27 +0100

    Implemented door detection and handling
    Doors are now detected and properly converted to a single segment,

--- scripts/DungeonScrawlImporterFormApplication.js
+++ scripts/DungeonScrawlImporterFormApplication.js
@@ -47,43 +47,80 @@ export class DungeonScrawlImporterFormApplication extends FormApplication {
         const sceneCellSize   = sceneDimensions.size
         const gridFactor      = sceneCellSize / gridCellSize
         for (let k in shapes) {
-            if (shapes.hasOwnProperty(k)) {
-                let isDoor = doorIds.includes(k)
-                let element   = shapes[k]
-                let validKeys = ['polygons', 'polylines']
-                validKeys.forEach((prop) => {
-                    if (element.hasOwnProperty(prop)) {
-                        element[prop].forEach(polygon => {
-                            let lastPoint = []
-                            let list
-                            if (prop === 'polygons') {
-                                list = polygon[0]
-                            }
-                            else {
-                                list = polygon
-                            }
-                            list.forEach(side => {
-                                let coordinates = [
-                                    side[0] * gridFactor,
-                                    side[1] * gridFactor
-                                ]
-                                if (lastPoint.length > 0) {
-                                    wallData.push({
-                                                      c: [
-                                                          lastPoint[0],
-                                                          lastPoint[1],
-                                                          coordinates[0],
-                                                          coordinates[1],
-                                                      ],
-                                                      'Door': isDoor ? CONST.WALL_DOOR_TYPES.DOOR : CONST.WALL_DOOR_TYPES.NONE
-                                                  })
-                                }
-                                lastPoint = coordinates
-                            })
-                        })
+            let isDoor = doorIds.includes(k)
+            if (!shapes.hasOwnProperty(k)) continue
+            let element   = shapes[k]
+            if (isDoor) {
+                let doorVariant = 1
+                if (element.polylines.length > 0) doorVariant = 0
+
+                if (doorVariant == 0) {
+                    delete element.polygons
+                    element.polylines = [[element.polylines[0][0], element.polylines[1][1]]]
+                }
+                else if (doorVariant == 1) {
+                    let doorLengths2 = []
+                    for (let i = 0; i < element.polygons[0][0].length; i++) {
+                        let doorLength2 = ((element.polygons[0][0][(i+1)%(element.polygons[0][0].length)][0] - element.polygons[0][0][i][0])**2 + (element.polygons[0][0][(i+1)%(element.polygons[0][0].length)][1] - element.polygons[0][0][i][1])**2)
+                        doorLengths2.push(doorLength2)
                     }
-                })
+                    let longestSideIndex = doorLengths2.indexOf(Math.max(...doorLengths2))
+                    let longestSide = [[element.polygons[0][0][longestSideIndex][0], element.polygons[0][0][longestSideIndex][1]],[element.polygons[0][0][(longestSideIndex+1)%(element.polygons[0][0].length)][0], element.polygons[0][0][(longestSideIndex+1)%(element.polygons[0][0].length)][1]]]
+                    let shortestSide = [[element.polygons[0][0][(longestSideIndex+1)%(element.polygons[0][0].length)][0], element.polygons[0][0][(longestSideIndex+1)%(element.polygons[0][0].length)][1]],[element.polygons[0][0][(longestSideIndex+2)%(element.polygons[0][0].length)][0], element.polygons[0][0][(longestSideIndex+2)%(element.polygons[0][0].length)][1]]]
+                    let shortestSideMidPoint = [(shortestSide[0][0] + shortestSide[1][0])/2, (shortestSide[0][1] + shortestSide[1][1])/2]
+                    let offset = [shortestSide[0][0] - shortestSideMidPoint[0], shortestSide[0][1] - shortestSideMidPoint[1]]
+                    element.polygons[0][0] = [[longestSide[0][0] - offset[0], longestSide[0][1] - offset[1]],[longestSide[1][0] - offset[0], longestSide[1][1] - offset[1]]]
+                }
             }
+            let validKeys = ['polygons', 'polylines']
+            validKeys.forEach((prop) => {
+                if (element.hasOwnProperty(prop)) {
+                    element[prop].forEach(polygon => {
+                        let firstPoint = []
+                        let lastPoint = []
+                        let list
+                        if (prop === 'polygons') {
+                            list = polygon[0]
+                        }
+                        else {
+                            list = polygon
+                        }
+                        list.forEach(side => {
+                            let coordinates = [
+                                side[0] * gridFactor,
+                                side[1] * gridFactor
+                            ]
+                            if (lastPoint.length == 0 && prop == 'polygons') {
+                                firstPoint = coordinates
+                            }
+                            if (lastPoint.length > 0) {
+                                wallData.push({
+                                                  c: [
+                                                      lastPoint[0],
+                                                      lastPoint[1],
+                                                      coordinates[0],
+                                                      coordinates[1],
+                                                  ],
+                                                  'door': isDoor
+                                              })
+                            }
+                            lastPoint = coordinates
+                        })
+                        if (prop == 'polygons') {
+                            wallData.push({
+                                c: [
+                                    lastPoint[0],
+                                    lastPoint[1],
+                                    firstPoint[0],
+                                    firstPoint[1],
+                                ],
+                                'door': isDoor
+                            })
+                        }
+                    })
+                }
+            })
+            
         }
 
         // Round to 3 decimals
@@ -91,7 +128,6 @@ export class DungeonScrawlImporterFormApplication extends FormApplication {
             element.c = element.c.map(value => Math.round(value * 100) / 100)
             return element
         })
-
         wallData = wallData.filter((value) => {
             return value.c[0] !== value.c[2] || value.c[1] !== value.c[3]
         })
@@ -106,7 +142,7 @@ export class DungeonScrawlImporterFormApplication extends FormApplication {
             uniqueKeys[key] = null
             return true
         })
-
+        
         let mostNegativeX = Math.min(...uniqueWallData.map(value => value.c[0]), ...uniqueWallData.map(value => value.c[2]))
         let mostNegativeY = Math.min(...uniqueWallData.map(value => value.c[1]), ...uniqueWallData.map(value => value.c[3]))
 
@@ -117,7 +153,6 @@ export class DungeonScrawlImporterFormApplication extends FormApplication {
             value.c[3] = value.c[3] + sceneCellSize * offsetY + mostNegativeY * -1 + sceneDimensions.sceneY
             return value
         })
-
         // noinspection JSUnresolvedFunction
         await game.scenes.current.createEmbeddedDocuments("Wall", uniqueWallData)
     }
@@ -125,9 +160,9 @@ export class DungeonScrawlImporterFormApplication extends FormApplication {
      * @param {Object} dataNodes
      */
     getDoorIds(dataNodes) {
-        let doorIds = dataNodes
+        let doorIds = []
         for (let node in dataNodes) {
-            if (node.name === "Door") doorIds.push(node.id)
+            if (dataNodes[node].name == "Door geometry") doorIds.push(dataNodes[node].geometryId)
         }
         return doorIds
     }
