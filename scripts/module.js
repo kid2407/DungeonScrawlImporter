cc1f60f8713f501cb04e1b1a21459f9fab1785ad
anders.tech <andersnelson4@gmail.com>
2023-07-06 14:46:11 -0700

    Fixed DS import button not showing in Foundry v11

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,7 @@
 import {DungeonScrawlImporterFormApplication} from "./DungeonScrawlImporterFormApplication.js"
 
 Hooks.on('renderSidebarTab', async function (app, html) {
-    if (app.options.id === "scenes" && game.user.isGM) {
+    if (app.options.classes.includes("scenes-sidebar") && game.user.isGM) {
         let button = $(`<div class='header-actions action-buttons flexrow'><button><i class='fas fa-file-import'></i> ${game.i18n.localize("DSI.button")}</button></div>`)
         button.on('click', async () => {
             if (!game.users.current.isGM) {
