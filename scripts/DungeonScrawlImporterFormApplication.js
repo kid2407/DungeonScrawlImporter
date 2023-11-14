1849d08901df8e0a0190bbe98bca0afcd5d61458
Tobias Franz <cinex.mail@web.de>
2023-11-14 15:58:37 +0100

    * Fixed a typo * Cleaned up code * Support for importing lights (if using Dungeon Scrawl Pro subscription)
    Signed-off-by: Tobias Franz <tobias.franz@mydata.de>

--- scripts/DungeonScrawlImporterFormApplication.js
+++ scripts/DungeonScrawlImporterFormApplication.js
@@ -35,10 +35,11 @@ export class DungeonScrawlImporterFormApplication extends FormApplication {
     /**
      * @param {Object} shapes
      * @param {Array} doorIDs
-     * @param {Array} stairIDs
+     * @param {Array} stairsIds
      * @param {int} gridCellSize
      * @param {int} offsetX
      * @param {int} offsetY
+     * @return {{xOffset: number, yOffset: number}}
      */
     async parseShapes(shapes, doorIDs, stairsIds, gridCellSize, offsetX, offsetY) {
         let wallData          = []
@@ -50,36 +51,36 @@ export class DungeonScrawlImporterFormApplication extends FormApplication {
         for (let k in shapes) {
             let isDoor = doorIDs.includes(k)
             if (!shapes.hasOwnProperty(k)) continue
-            let element   = shapes[k]
+            let element = shapes[k]
             //If the element is a door the shape has to be reduced to a single segment to work properly as a foundry door.
             if (isDoor) {
                 let doorVariant = 1
                 if (element.polylines.length > 0) doorVariant = 0
 
                 //For this variant the desired segment can be constructed by simply connecting the first point of the first polyline and the last point of the second polyline.
-                if (doorVariant == 0) {
+                if (doorVariant === 0) {
                     delete element.polygons
                     element.polylines = [[element.polylines[0][0], element.polylines[1][1]]]
                 }
                 //For this variant the desired segment can be constructed by shifting one of the long edges of the rectangular polygon.
-                else if (doorVariant == 1) {
+                else if (doorVariant === 1) {
                     //Determine the side lengths of the rectangular polygon.
                     let doorLengths2 = []
                     for (let i = 0; i < element.polygons[0][0].length; i++) {
-                        let doorLength2 = ((element.polygons[0][0][(i+1)%(element.polygons[0][0].length)][0] - element.polygons[0][0][i][0])**2 + (element.polygons[0][0][(i+1)%(element.polygons[0][0].length)][1] - element.polygons[0][0][i][1])**2)
+                        let doorLength2 = ((element.polygons[0][0][(i + 1) % (element.polygons[0][0].length)][0] - element.polygons[0][0][i][0]) ** 2 + (element.polygons[0][0][(i + 1) % (element.polygons[0][0].length)][1] - element.polygons[0][0][i][1]) ** 2)
                         doorLengths2.push(doorLength2)
                     }
                     //Grab one of the long sides of the rectangular polygon.
                     let longestSideIndex = doorLengths2.indexOf(Math.max(...doorLengths2))
-                    let longestSide = [[element.polygons[0][0][longestSideIndex][0], element.polygons[0][0][longestSideIndex][1]],[element.polygons[0][0][(longestSideIndex+1)%(element.polygons[0][0].length)][0], element.polygons[0][0][(longestSideIndex+1)%(element.polygons[0][0].length)][1]]]
+                    let longestSide      = [[element.polygons[0][0][longestSideIndex][0], element.polygons[0][0][longestSideIndex][1]], [element.polygons[0][0][(longestSideIndex + 1) % (element.polygons[0][0].length)][0], element.polygons[0][0][(longestSideIndex + 1) % (element.polygons[0][0].length)][1]]]
 
                     //Grab the next side over relative to the long side. Since the shape is always a rectangle this must be a short side.
-                    let shortestSide = [[element.polygons[0][0][(longestSideIndex+1)%(element.polygons[0][0].length)][0], element.polygons[0][0][(longestSideIndex+1)%(element.polygons[0][0].length)][1]],[element.polygons[0][0][(longestSideIndex+2)%(element.polygons[0][0].length)][0], element.polygons[0][0][(longestSideIndex+2)%(element.polygons[0][0].length)][1]]]
+                    let shortestSide         = [[element.polygons[0][0][(longestSideIndex + 1) % (element.polygons[0][0].length)][0], element.polygons[0][0][(longestSideIndex + 1) % (element.polygons[0][0].length)][1]], [element.polygons[0][0][(longestSideIndex + 2) % (element.polygons[0][0].length)][0], element.polygons[0][0][(longestSideIndex + 2) % (element.polygons[0][0].length)][1]]]
                     //Determine the x,y offset, which is the difference between a point and a midway point along the short side.
-                    let shortestSideMidPoint = [(shortestSide[0][0] + shortestSide[1][0])/2, (shortestSide[0][1] + shortestSide[1][1])/2]
-                    let offset = [shortestSide[0][0] - shortestSideMidPoint[0], shortestSide[0][1] - shortestSideMidPoint[1]]
+                    let shortestSideMidPoint = [(shortestSide[0][0] + shortestSide[1][0]) / 2, (shortestSide[0][1] + shortestSide[1][1]) / 2]
+                    let offset               = [shortestSide[0][0] - shortestSideMidPoint[0], shortestSide[0][1] - shortestSideMidPoint[1]]
                     //Set the element's polyline to this single line segment, which is one of the long sides shifted by the previously determined offset along a short side.
-                    element.polylines[0] = [[longestSide[0][0] - offset[0], longestSide[0][1] - offset[1]],[longestSide[1][0] - offset[0], longestSide[1][1] - offset[1]]]
+                    element.polylines[0]     = [[longestSide[0][0] - offset[0], longestSide[0][1] - offset[1]], [longestSide[1][0] - offset[0], longestSide[1][1] - offset[1]]]
                     //Delete the polygon.
                     delete element.polygons
                 }
@@ -90,9 +91,9 @@ export class DungeonScrawlImporterFormApplication extends FormApplication {
                 element['polylines'].forEach((side) => {
                     //Each sequential pair of points is a possible line, and
                     //some of them won't have any length, so won't appear
-                    for(let i = 0; i + 1 < side.length; i = i + 1) {
+                    for (let i = 0; i + 1 < side.length; i = i + 1) {
                         wallData.push({
-                            c: [ side[i][0] * gridFactor, side[i][1] * gridFactor, side[i+1][0] * gridFactor, side[i+1][1] * gridFactor ],
+                            c:      [side[i][0] * gridFactor, side[i][1] * gridFactor, side[i + 1][0] * gridFactor, side[i + 1][1] * gridFactor],
                             'door': isDoor,
                         })
                     }
@@ -102,31 +103,31 @@ export class DungeonScrawlImporterFormApplication extends FormApplication {
                 element['polygons'].forEach(polygon => {
                     polygon.forEach(list => {
                         let firstPoint = []
-                        let lastPoint = []
+                        let lastPoint  = []
                         list.forEach(side => {
                             let coordinates = [
                                 side[0] * gridFactor,
                                 side[1] * gridFactor
                             ]
                             //Save the first point to connect with the last point later
-                            if (lastPoint.length == 0) {
+                            if (lastPoint.length === 0) {
                                 firstPoint = coordinates
                             }
                             if (lastPoint.length > 0) {
                                 wallData.push({
-                                                  c: [
-                                                      lastPoint[0],
-                                                      lastPoint[1],
-                                                      coordinates[0],
-                                                      coordinates[1],
-                                                  ],
-                                                  'door': isDoor
-                                              })
+                                    c:      [
+                                        lastPoint[0],
+                                        lastPoint[1],
+                                        coordinates[0],
+                                        coordinates[1],
+                                    ],
+                                    'door': isDoor
+                                })
                             }
                             lastPoint = coordinates
                         })
                         wallData.push({
-                            c: [
+                            c:      [
                                 lastPoint[0],
                                 lastPoint[1],
                                 firstPoint[0],
@@ -163,23 +164,29 @@ export class DungeonScrawlImporterFormApplication extends FormApplication {
         let mostNegativeX = Math.min(...uniqueWallData.map(value => value.c[0]), ...uniqueWallData.map(value => value.c[2]))
         let mostNegativeY = Math.min(...uniqueWallData.map(value => value.c[1]), ...uniqueWallData.map(value => value.c[3]))
 
+        let calculatedXOffset = sceneCellSize * offsetX + mostNegativeX * -1 + sceneDimensions.sceneX;
+        let calculatedYOffset = sceneCellSize * offsetY + mostNegativeY * -1 + sceneDimensions.sceneY;
+
         uniqueWallData = uniqueWallData.map(value => {
-            value.c[0] = value.c[0] + sceneCellSize * offsetX + mostNegativeX * -1 + sceneDimensions.sceneX
-            value.c[1] = value.c[1] + sceneCellSize * offsetY + mostNegativeY * -1 + sceneDimensions.sceneY
-            value.c[2] = value.c[2] + sceneCellSize * offsetX + mostNegativeX * -1 + sceneDimensions.sceneX
-            value.c[3] = value.c[3] + sceneCellSize * offsetY + mostNegativeY * -1 + sceneDimensions.sceneY
+            value.c[0] = value.c[0] + calculatedXOffset
+            value.c[1] = value.c[1] + calculatedYOffset
+            value.c[2] = value.c[2] + calculatedXOffset
+            value.c[3] = value.c[3] + calculatedYOffset
             return value
         })
         // noinspection JSUnresolvedFunction
         await game.scenes.current.createEmbeddedDocuments("Wall", uniqueWallData)
+
+        return {xOffset: calculatedXOffset, yOffset: calculatedYOffset}
     }
+
     /**
      * @param {Object} dataNodes
      */
     getDoorIDs(dataNodes) {
         let doorIDs = []
         for (let node in dataNodes) {
-            if (dataNodes[node].name == "Door geometry") doorIDs.push(dataNodes[node].geometryId)
+            if (dataNodes[node].name === "Door geometry") doorIDs.push(dataNodes[node].geometryId)
         }
         return doorIDs
     }
@@ -190,9 +197,54 @@ export class DungeonScrawlImporterFormApplication extends FormApplication {
     getStairIDs(dataNodes) {
         let stairIDs = []
         for (let node in dataNodes) {
-            if (dataNodes[node].name == "Stairs geometry") stairIDs.push(dataNodes[node].geometryId)
+            if (dataNodes[node].name === "Stairs geometry") stairIDs.push(dataNodes[node].geometryId)
         }
         return stairIDs
+    }
+
+    /**
+     * @param {Object} dataNodes
+     */
+    getLights(dataNodes) {
+        let lightIDs = []
+        for (let node in dataNodes) {
+            if (dataNodes[node].type === "LIGHT") lightIDs.push(dataNodes[node])
+        }
+        return lightIDs
+    }
+
+    /**
+     * @param {Array} lightData
+     * @param {{xOffset: number, yOffset: number}} offsetData
+     * @param {number} gridCellSize
+     */
+    async processLights(lightData, offsetData, gridCellSize) {
+        let lights      = []
+        let data        = []
+        let lightType   = ''
+        let lightConfig = {}
+
+        for (let key in lightData) {
+            data        = lightData[key]
+            lightType   = data["variant"]
+            lightConfig = data.config
+
+            let light = {
+                x:      data.transform[4] * (game.scenes.current.grid.size / gridCellSize) + offsetData.xOffset,
+                y:      data.transform[5] * (game.scenes.current.grid.size / gridCellSize) + offsetData.yOffset,
+                config: {
+                    alpha:      data.alpha,
+                    bright:     lightConfig[lightType].radius,
+                    color:      "#" + lightConfig.shared.colour.colour.toString(16).padStart(6, "0"),
+                    dim:        lightConfig[lightType].radius,
+                    hidden:     !data.visible,
+                    luminosity: lightConfig[lightType].strength,
+                }
+            }
+            lights.push(light)
+        }
+
+        await game.scenes.current.createEmbeddedDocuments("AmbientLight", lights)
     }
 
     /**
@@ -214,14 +266,15 @@ export class DungeonScrawlImporterFormApplication extends FormApplication {
             let dataNodes    = data.state.document.nodes
             let doorIDs      = self.getDoorIDs(dataNodes)
             let stairIDs     = self.getStairIDs(dataNodes)
+            let lightData    = self.getLights(dataNodes)
             let doc          = dataNodes.document
             let gridCellSize = dataNodes[doc.children[0]].grid.cellDiameter
 
             self.updateButtonStatus(game.i18n.localize("DSI.import.parsingfile"), true)
-            await self.parseShapes(geometryData, doorIDs, stairIDs, gridCellSize, offsetX, offsetY)
+            let offsetData = await self.parseShapes(geometryData, doorIDs, stairIDs, gridCellSize, offsetX, offsetY)
+            await self.processLights(lightData, offsetData, gridCellSize)
             self.updateButtonStatus(game.i18n.localize("DSI.import.finished"))
-        }
-        else {
+        } else {
             $('#dsimportErrorMessage').html(game.i18n.localize("DSI.import.error.noFile")).addClass("visible")
             self.updateButtonStatus(game.i18n.localize("DSI.import.import"))
         }
